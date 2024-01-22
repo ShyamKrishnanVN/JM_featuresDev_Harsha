762d3ed9ff5cd9ba25509108a72d7a7bad0a3323
harshanyapathi <harshanyapathi>
2024-01-22 16:23:47 +0530

    updating files

--- util1.py
+++ util1.py
@@ -1,3 +1,3 @@
 import pandas as pd
 def func(num):
-    return pd.DataFrame({'col1':[i for i in range(num)],'col2':[i*i*i for i in range(num)]})
+    return pd.DataFrame({'col1':[i for i in range(num)],'col2':['main' for i in range(num)]})
